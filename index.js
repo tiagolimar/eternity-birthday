ca3a38009cc62a8013e496b1d3c0f3349d0cd3cd
tiagolimar <tiagolima.r2@gmail.com>
2025-05-16 21:10:51 -0300

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 async function verify(){
     const response = await fetch(process.env.URL_API);
-    const result = response.json();
+    const result = await response.json();
     console.log(result);
 }
 
