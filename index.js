2af1478a66a681815d40d41343d3fd7a0f78d79b
tiagolimar <tiagolima.r2@gmail.com>
2025-05-16 21:30:40 -0300

    fix bug array compare

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ async function verify(){
     const response = await fetch(process.env.URL_API);
     const result = await response.json();
 
-    if (result == []) console.log('Parece que não há aniversariantes hoje');
-    else console.log('Aniversariantes do dia:', result.join(', '));
+    if (isArray(result) && result.length === 0) {
+        console.log('Parece que não há aniversariantes hoje');
+    } else {
+        console.log('Aniversariantes do dia:', result.join(', '));
+    }
 }
 
 verify();
