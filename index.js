0fd481bf83fa73f4f633be652605e4265e057066
tiagolimar <tiagolima.r2@gmail.com>
2025-05-16 21:19:07 -0300

    update msgs

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import 'dotenv/config';
 async function verify(){
     const response = await fetch(process.env.URL_API);
     const result = await response.json();
-    console.log(result);
+
+    if (result == []) console.log('Parece que não há aniversariantes hoje');
+    else console.log('Aniversariantes do dia:', result.join(', '));
 }
 
 verify();
